86989ca1ba3761377f940ea0121577a304fe389b
cybex-dev <k4gcybex@gmail.com>
2020-03-01 20:23:10 +0200

    Final Update

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,83 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * See for more details: https://howtodoinjava.com/library/json-simple-read-write-json-examples/
+ */
 public class Main {
+    private String clazz;
+    private Long totalStudents;
+    private boolean isAtCapacity;
+    private double average;
+    private List<Student> students;
+    private Long year;
+
+    class Student {
+        private String name, surname;
+        private boolean isMale;
+        private Long age;
+        private List<Long> marks;
+
+        public Student() {
+        }
+
+        public Student(String name, String surname, boolean isMale, Long age, List<Long> marks) {
+            this.name = name;
+            this.surname = surname;
+            this.isMale = isMale;
+            this.age = age;
+            this.marks = marks;
+        }
+
+        public List<Long> getMarks() {
+            return marks;
+        }
+
+        public void setMarks(List<Long> marks) {
+            this.marks = marks;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public boolean getIsMale() {
+            return isMale;
+        }
+
+        public void setIsMale(boolean male) {
+            isMale = male;
+        }
+
+        public Long getAge() {
+            return age;
+        }
+
+        public void setAge(Long age) {
+            this.age = age;
+        }
+    }
+
     public static void main(String[] args) {
         Main main = new Main();
 
@@ -30,17 +101,17 @@ public class Main {
         main.editJSONContent();
 
         // Write out to JSON file
-        main.writeToJSON(jsonFile);
+        main.writeToJSON("output.json");
     }
 
     private Main readFromJSON(String jsonFile) {
         // Setup reader to read JSon file
         JSONParser jsonParser = new JSONParser();
+        students = new ArrayList<>();
 
         JSONObject obj = null;
-        try (FileReader reader = new FileReader("employees.json"))
+        try (FileReader reader = new FileReader(jsonFile))
         {
-            System.out.println("Reading JSON file");
             //Read JSON file
             obj = (JSONObject) jsonParser.parse(reader);
 
@@ -52,42 +123,116 @@ public class Main {
         }
 
         // Reading number of students
-        Integer totalStudents = (Integer) obj.get("totalStudents");
+        totalStudents = (Long) obj.get("totalStudents");
 
         // Read capacity state
-        Boolean isAtCapacity = (Boolean) obj.get("atCapacity");
+        isAtCapacity = (Boolean) obj.get("atCapacity");
 
         // Read average
-        Float average = (Float) obj.get("average");
+        average = (Double) obj.get("average");
+
+        // Read Class
+        clazz = (String) obj.get("class");
+
+        // Read Year
+        year = (Long) obj.get("year");
 
         // Read students from JSON file
-        
+        JSONArray studentArray = (JSONArray) obj.get("students");
+        for (Object o : studentArray) {
 
+            // Create student object
+            JSONObject jsonObject = (JSONObject) o;
+            Student student = new Student();
+            student.setName((String) jsonObject.getOrDefault("name", "n/a"));
+            student.setSurname((String) jsonObject.getOrDefault("surname", "n/a"));
+            student.setAge((Long) jsonObject.getOrDefault("age", 0));
+            student.setIsMale((Boolean) jsonObject.getOrDefault("isMale", true));
+
+            // Process marks array
+            JSONArray studentMarks = (JSONArray) jsonObject.get("marks");
+            List<Long> marks = new ArrayList<>();
+            // map all objects to integers and add to list
+            for (Object studentMark : studentMarks) {
+                marks.add((Long) studentMark);
+            }
+            student.setMarks(marks);
+
+            // add student
+            students.add(student);
+        }
 
         return this;
     }
 
+    private Double getAverageFromList(List<Long> marks) {
+        return marks.stream().mapToInt(Long::intValue).average().orElse(0);
+    }
+
     private Main editJSONContent() {
         // Edit bill gates to be older
+        Student billGates = students.stream().filter(student -> student.getName().equals("bill") && student.getSurname().equals("gates")).findFirst().orElse(null);
+        if (billGates != null) {
+            billGates.setAge(1234L);
+        }
 
         // Add Student
+        students.add(new Student("Jane", "Doe", false, 25L, Arrays.asList(27L, 77L, 99L)));
 
         // Set class capacity full to true
+        isAtCapacity = true;
 
         // Change class average
+        average = students.stream().mapToDouble(value -> getAverageFromList(value.getMarks())).average().orElse(0);
 
         return this;
     }
 
     private Main writeToJSON(String jsonFile) {
-        // Setup writer to write to JSON file
+        JSONObject rootObject = new JSONObject();
 
-        // Write students to file
+        // Write class
+        rootObject.put("class", clazz);
 
-        // Write new average
+        // Write totalStudents
+        rootObject.put("totalStudents", totalStudents);
 
         // Write new capacity state
+        rootObject.put("atCapacity", isAtCapacity);
 
+        // Write students to file
+        JSONArray studentArray = new JSONArray();
+        students.forEach(student -> {
+            // Creat containing JSON object
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("name", student.getName());
+            jsonObject.put("surname", student.getSurname());
+            jsonObject.put("age", student.getAge());
+            jsonObject.put("isMale", student.getIsMale());
+
+            // Get Student's marks
+            jsonObject.put("marks", student.getMarks());
+
+            // Add Student JSON object to Students JSON Array
+            studentArray.add(jsonObject);
+        });
+        rootObject.put("students", studentArray);
+
+        // Write year
+        rootObject.put("year", year);
+
+        // Write new average
+        rootObject.put("average", average);
+
+        // Setup writer to write to JSON file
+        try (FileWriter writer = new FileWriter(jsonFile))
+        {
+            //Read JSON file
+            writer.write(rootObject.toJSONString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return this;
     }
 
